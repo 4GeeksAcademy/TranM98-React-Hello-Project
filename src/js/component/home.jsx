1bfd2d465bf574a96b3069f6cde64d5d666797d6
Mai Tran <114268221+tranm98@users.noreply.github.com>
2023-04-25 00:48:23 +0000

    first draft, still incomplete, error

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,86 @@
-import React from "react";
+import React, {useState} from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
-//create your first component
-const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+import "./styles.css";
+
+const App = () => {
+	const [task, setTask] = useState([]);
+	const [inputVal, setInputVal] = useState('');
+
+	const inputChangeHandle = (event) => {
+		setInputVal(event.target.value);
+	};
+
+	const AddTask = () => {
+		if(inputVal.trim() ==== ''){
+			return;
+		}
+		setTask([...task, {title: inputVal.trim(), completed: false }]);
+		setInputVal('');
+	};
+	const DeleteTask = (index) => {
+		setTask(task.filter((task, i) => i !== index));
+	};
+	const toggleComplete = (index) => {
+		setTask(
+			task.map((task, i)) =>
+			i === index ? {...task, completed: !task.completed} : task)
+		);
+	};
 };
 
-export default Home;
+const incompleteTask = task.filter((task) => !task.completed).length;
+
+return(
+	//Div for App
+	<div className="todo-app">
+		<h1>todos</h1>
+		<div className="todo-box">
+			<input>
+			type="text"
+			placeholder="What needs to be done?"
+			value={inputVal}
+			onChange={inputChangeHandle}
+			onKeyPress={(event)=> {
+				if (event.key === 'Enter'){
+					AddTask();
+				}
+			}}
+			</input>
+			<button onClick = {AddTask}>Add</button>
+			<div className="task-list">
+				{task.length === 0 ? (
+					<p>No tasks, add a task</p>
+					): (
+					task.map((task, index) => (
+						<div
+							key={index}
+							className={'task ${task.completed ? 'completed' : ''}'}>
+								<p>{task.title}</p>
+								<div className ="task-button">
+									<span onClick = {() => toggleComplete(index)}>
+										{task.completed ? 'Mark Incomplete' : 'Mark Complete'}
+									</span>
+
+									<span onClick = { () => DeleteTask(index)}>
+										X
+									</span>
+								</div>
+						</div>
+
+					))
+					)}
+
+			</div>
+			{task.length > 0 && (
+				<p className="task-count">{incompleteTask} tasks left</p>
+			)}
+		</div>
+	</div>
+	// ^Closing Div for App (Do not delete this div, pls)
+);
+}
+
+export default App;
