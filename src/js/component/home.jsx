3764049d4ef37486c0226c6561d76c39ce4b233d
Mai Tran <114268221+tranm98@users.noreply.github.com>
2023-04-26 01:04:58 +0000

    close enough??

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,86 +1,72 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-import "./styles.css";
 
 const App = () => {
-	const [task, setTask] = useState([]);
-	const [inputVal, setInputVal] = useState('');
+  const [tasks, setTasks] = useState([]);
+  const [inputValue, setInputValue] = useState("");
 
-	const inputChangeHandle = (event) => {
-		setInputVal(event.target.value);
-	};
+  const inputChange = (event) => {
+    setInputValue(event.target.value);
+  };
 
-	const AddTask = () => {
-		if(inputVal.trim() ==== ''){
-			return;
-		}
-		setTask([...task, {title: inputVal.trim(), completed: false }]);
-		setInputVal('');
-	};
-	const DeleteTask = (index) => {
-		setTask(task.filter((task, i) => i !== index));
-	};
-	const toggleComplete = (index) => {
-		setTask(
-			task.map((task, i)) =>
-			i === index ? {...task, completed: !task.completed} : task)
-		);
-	};
-};
+  const addTask = () => {
+    if (inputValue.trim() === "") {
+      return;
+    }
+    setTasks([...tasks, { title: inputValue.trim(), completed: false }]);
+    setInputValue("");
+  };
 
-const incompleteTask = task.filter((task) => !task.completed).length;
+  const deleteTask = (index) => {
+    setTasks(tasks.filter((task, i) => i !== index));
+  };
 
-return(
-	//Div for App
-	<div className="todo-app">
-		<h1>todos</h1>
-		<div className="todo-box">
-			<input>
-			type="text"
-			placeholder="What needs to be done?"
-			value={inputVal}
-			onChange={inputChangeHandle}
-			onKeyPress={(event)=> {
-				if (event.key === 'Enter'){
-					AddTask();
-				}
-			}}
-			</input>
-			<button onClick = {AddTask}>Add</button>
-			<div className="task-list">
-				{task.length === 0 ? (
-					<p>No tasks, add a task</p>
-					): (
-					task.map((task, index) => (
-						<div
-							key={index}
-							className={'task ${task.completed ? 'completed' : ''}'}>
-								<p>{task.title}</p>
-								<div className ="task-button">
-									<span onClick = {() => toggleComplete(index)}>
-										{task.completed ? 'Mark Incomplete' : 'Mark Complete'}
-									</span>
+  const toggleComplete = (index) => {
+    setTasks(
+      tasks.map((task, i) =>
+        i === index ? { ...task, completed: !task.completed } : task
+      )
+    );
+  };
 
-									<span onClick = { () => DeleteTask(index)}>
-										X
-									</span>
-								</div>
-						</div>
+  const incompleteTask = tasks.filter((task) => !task.completed).length;
 
-					))
-					)}
-
-			</div>
-			{task.length > 0 && (
-				<p className="task-count">{incompleteTask} tasks left</p>
-			)}
-		</div>
-	</div>
-	// ^Closing Div for App (Do not delete this div, pls)
-);
+  return (
+    <div className="App">
+      <h1>todos</h1>
+      <div className="todo-box">
+        <div className="input-container">
+        <input
+          type="text"
+          placeholder="What needs to be done?"
+          value={inputValue}
+          onChange={inputChange}
+          onKeyPress={(event) => {
+            if (event.key === "Enter") {
+              addTask();
+            }
+          }}
+        />
+        </div>
+        <div className="task-list">
+          {tasks.map((task, index) => (
+            <div key={index} className="task-item">
+              <span
+                className={task.completed ? "completed" : ""}
+                onClick={() => toggleComplete(index)}
+              >
+                {task.title}
+              </span>
+              <span className="delete-icon" onClick={() => deleteTask(index)}>
+                X
+              </span>
+            </div>
+          ))}
+        </div>
+        <div className="task-left">{incompleteTask} items left</div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
